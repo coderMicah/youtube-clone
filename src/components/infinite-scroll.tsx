a3aa08e60263b4729962bd315ead3c65252a252d
Micah <ismiki977@gmail.com>
2025-02-09 18:08:09 +0300

    feat:add infinite loading

---
+++ src/components/infinite-scroll.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { useIntersectionObserver } from '@/hooks/use-intersection-observer';
+
+interface InfiniteScrollProps {
+  isManual?: boolean;
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
+  fetchNextPage: () => void;
+}
+
+const InfiniteScroll = ({
+  isManual = false,
+  hasNextPage,
+  isFetchingNextPage,
+  fetchNextPage,
+}: InfiniteScrollProps) => {
+  const { targetRef, isIntersecting } = useIntersectionObserver({
+    threshold: 0.5,
+    rootMargin: '100px',
+  });
+
+  useEffect(() => {
+    if (isIntersecting && hasNextPage && !isFetchingNextPage && !isManual) {
+      fetchNextPage();
+    }
+  }, [isFetchingNextPage, isIntersecting, isManual, hasNextPage, fetchNextPage]);
+
+  return (
+    <div className="flex flex-col items-center gap-4 p-4">
+      <div ref={targetRef} className="h-1" />
+      {hasNextPage ? (
+        <Button
+          variant={'secondary'}
+          disabled={isFetchingNextPage || !hasNextPage}
+          onClick={() => fetchNextPage()}>
+          {isFetchingNextPage ? 'Loading...' : 'Load More'}
+        </Button>
+      ) : (
+        <p className="text-sm text-muted-foreground">You have reached the end of the list</p>
+      )}
+    </div>
+  );
+};
+
+export default InfiniteScroll;
